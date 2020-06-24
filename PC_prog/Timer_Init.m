bea471a4d2a391e84899a19333dc0b431e2123e5
stonecutter-hp <hongpengliao@outlook.com>
2020-06-24 16:16:04 +0800

    Update Timer_Init.m

--- PC_prog/Timer_Init.m
+++ PC_prog/Timer_Init.m
@@ -2,7 +2,8 @@ function Timer_Init(Main_Freq)
 global P;
 % create and set timer properties
 % 1/Main_Freq should be larger than 0.001
-Timer1 = timer('BusyMode','drop','ExecutionMode','fixedRate','Period',1/Main_Freq,'TimerFcn',@TimerCallback);  % remind the main frequency here
+% remind the main frequency here
+Timer1 = timer('BusyMode','drop','ExecutionMode','fixedRate','Period',1/Main_Freq,'TimerFcn',@TimerCallback);  
 %Timer1.TimerFcn = {@TimerCallback,P};  % set the callback of the timer
 P.config{2,1} = Timer1;  % store the configuration of timer1
 tic
